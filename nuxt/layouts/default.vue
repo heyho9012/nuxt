8cea2e4383584cdf2e89b88fff55202ec0c42a1c
heyho9012 <heyho9012@gmail.com>
2021-08-24 15:37:29 +0900

    Add nuxt project

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header id="header">
+      <h1>{{ $route.name }} page</h1>
+      <ul>
+        <li>
+          <NuxtLink to="/">홈페이지</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/main">메인페이지</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/product">상품페이지</NuxtLink>
+        </li>
+      </ul>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt/pages/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLogo />
+  </div>
+</template>
+
+<script>
+  import NuxtLogo from '@/components/NuxtLogo.vue';
+
+  export default {
+    components: {
+      NuxtLogo,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
